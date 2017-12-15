5ac208e66c32530c72501493f34e48cd4bbd4d83
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2017-12-15 13:16:46 +0100

    Modified code

--- scripts.js
+++ scripts.js
@@ -2,15 +2,16 @@ function Phone(brand, price, color) {
     this.brand = brand;
     this.price = price;
     this.color = color;
+    this.camera = camera;
 }
 
 Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "Camera is  " + this.camera + ".");
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var SamsungGalaxyS6 =new Phone("Samsung", 1534, "black");
-var OnePlusOne = new Phone("One Plus", 1299, "black");
+var iPhone6S = new Phone("Apple", 2250, "silver", "5.0 Mpix - przód, 12.0 Mpix - tył");
+var SamsungGalaxyS6 =new Phone("Samsung", 1534, "black", "16 Mpix - tył i 5 Mpix - przód");
+var OnePlusOne = new Phone("One Plus", 1299, "black", "13 Mpix");
 
 iPhone6S.printInfo();
 SamsungGalaxyS6.printInfo();
