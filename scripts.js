9f674336bc8e782e81f36dfbaa0babd5bb0ab7d6
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2017-12-20 20:19:57 +0100

    Add code to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,16 @@
-function Phone(brand, price, color, camera) {
-    this.brand = brand;
-    this.price = price;
-    this.color = color;
-    this.camera = camera;
+function Button (text){
+    this.text = text || 'Hello';
 }
-
-Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "Camera is  " + this.camera + ".");
+Button.prototype = {
+    create: function(){
+        var self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        this.$element.click(function(){
+            alert(self.text);
+        });
+        this.$element.appendTo($('body'));
+    }
 }
-
-var iPhone6S = new Phone("Apple", 2250, "silver", "5.0 Mpix - przód, 12.0 Mpix - tył");
-var SamsungGalaxyS6 =new Phone("Samsung", 1534, "black", "16 Mpix - tył i 5 Mpix - przód");
-var OnePlusOne = new Phone("One Plus", 1299, "black", "13 Mpix");
-
-iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
+var btn1 = new Button('Hello!');
+btn1.create();
